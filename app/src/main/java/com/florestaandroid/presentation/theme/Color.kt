6052f499cde334de5b0bd8cf479b6b3247bd2790
Joao Victor Sena <jvsena16@gmail.com>
2024-05-05 17:35:24 -0300

    feat: add colors

--- app/src/main/java/com/florestaandroid/presentation/theme/Color.kt
+++ app/src/main/java/com/florestaandroid/presentation/theme/Color.kt
@@ -2,10 +2,18 @@ package com.florestaandroid.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF007141)
+val PrimaryContainer = Color(0xFF8CC27A)
+val OnPrimary = Color(0xFFD9D9D9)
+val Secondary = Color(0xFFEAB100)
+val OnSecondary = Color(0xFF000000)
+val Surface = Color(0xFFD9D9D9)
+val OnSurface = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryDark = Color(0xFF212121)
+val PrimaryContainerDark = Color(0xFF141414)
+val OnPrimaryDark = Color(0xFFD9D9D9)
+val SecondaryDark = Color(0xFFEAB100)
+val OnSecondaryDark = Color(0xFF000000)
+val SurfaceDark = Color(0xFF141414)
+val OnSurfaceDark = Color(0xFFFFFFFF)
