8c6d0b35106b67b26fa0c6ed93afbc83228b175c
Joao Victor Sena <jvsena16@gmail.com>
2024-05-11 15:08:59 -0300

    feat: add spacing to material theme

--- app/src/main/java/com/florestaandroid/presentation/theme/Theme.kt
+++ app/src/main/java/com/florestaandroid/presentation/theme/Theme.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalContext
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -274,6 +276,7 @@ fun FlorestaAndroidTheme(
         darkTheme -> darkScheme
         else -> lightScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
@@ -282,10 +285,11 @@ fun FlorestaAndroidTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography,
-        content = content
-    )
+    CompositionLocalProvider(value = LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = AppTypography,
+            content = content
+        )
+    }
 }
