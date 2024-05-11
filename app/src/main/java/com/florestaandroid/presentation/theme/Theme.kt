f8faaa92b44a428259c14174696f45927e782056
Joao Victor Sena <jvsena16@gmail.com>
2024-05-11 17:01:09 -0300

    fix: remove dinamic theme

--- app/src/main/java/com/florestaandroid/presentation/theme/Theme.kt
+++ app/src/main/java/com/florestaandroid/presentation/theme/Theme.kt
@@ -268,10 +268,10 @@ fun FlorestaAndroidTheme(
     content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> darkScheme
         else -> lightScheme
