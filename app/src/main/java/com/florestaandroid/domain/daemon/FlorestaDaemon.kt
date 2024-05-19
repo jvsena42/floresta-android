7068312b1239325451d2bd0a4f89d4e1fe293982
Joao Victor Sena <jvsena16@gmail.com>
2024-05-19 15:59:17 -0300

    feat: node client wip

---
+++ app/src/main/java/com/florestaandroid/domain/daemon/FlorestaDaemon.kt
@@ -0,0 +1,41 @@
+package com.florestaandroid.domain.daemon
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import uniffi.floresta.Config
+import uniffi.floresta.Florestad
+import uniffi.floresta.Network
+import javax.inject.Inject
+
+interface FlorestaDaemon {
+    fun start()
+    fun stop()
+}
+
+class FlorestaDaemonImpl @Inject constructor(
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    private val application: Application
+) :FlorestaDaemon {
+
+    private lateinit var daemon: Florestad
+    override fun start() {
+        val datadir = application.dataDir.toString()
+        val config = Config(
+            false,
+            listOf(),
+            true,
+            Network.SIGNET,
+            datadir,
+            electrumAddress = "127.0.0.1:50001",
+            logToFile = true
+        )
+        daemon = Florestad.fromConfig(config)
+        daemon.start()
+    }
+
+    override fun stop() {
+        daemon.stop()
+    }
+
+}
