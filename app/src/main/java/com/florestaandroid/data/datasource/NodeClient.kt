d09fa327df1b0cee4e0d573b11065f96ee77e531
Joao Victor Sena <jvsena16@gmail.com>
2024-05-20 14:26:57 -0300

    feat: create node client

--- app/src/main/java/com/florestaandroid/data/datasource/NodeClient.kt
+++ app/src/main/java/com/florestaandroid/data/datasource/NodeClient.kt
@@ -1,6 +1,5 @@
 package com.florestaandroid.data.datasource
 
-import android.app.Application
 import com.florestaandroid.data.dto.request.ElectrumRequest
 import com.florestaandroid.data.dto.response.GetBalanceResponse
 import com.florestaandroid.data.toHexString
@@ -9,8 +8,6 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -38,20 +35,24 @@ class NodeClient @Inject constructor(
 
     suspend fun getBalance(
         xPubKey: String
-    ): Flow<GetBalanceResponse> = flow {
+    ): Flow<Result<GetBalanceResponse>> = flow {
         withContext(dispatcher) {
-            client.outputStream.write(
-                Gson().toJson(
-                    ElectrumRequest(
-                        method = BlockchainMethods.GET_BALANCE.method,
-                        params = listOf(xPubKey.toByteArray().toHexString()) //TODO CHECK IF THIS IS RIGHT
-                    )
-                ).toByteArray()
-            )
+            try {
+                client.outputStream.write(
+                    Gson().toJson(
+                        ElectrumRequest(
+                            method = BlockchainMethods.GET_BALANCE.method,
+                            params = listOf(xPubKey.toByteArray().toHexString()) //TODO CHECK IF THIS IS RIGHT
+                        )
+                    ).toByteArray()
+                )
 
-            val text = BufferedReader(InputStreamReader(client.inputStream)).readLine()
-            val result = Gson().fromJson(text, GetBalanceResponse::class.java)
-            emit(result)
+                val text = BufferedReader(InputStreamReader(client.inputStream)).readLine()
+                val result = Gson().fromJson(text, GetBalanceResponse::class.java)
+                emit(Result.success(result))
+            } catch (e : Exception) {
+                emit(Result.failure(e))
+            }
         }
     }
 
